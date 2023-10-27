d63ee3f2081065ef20bcf0a875f055c924d7681b
Ahsankhan556 <ak5589667@gmail.com>
2023-10-27 23:15:22 +1030

    SMIT ASIMINT JAVASCRIPT

---
+++ app.js
@@ -0,0 +1,115 @@
+                            //  ? Chapter No : 38 To 42
+                                    // ! Q  No 1 
+
+// function power(a, b) {
+//   return Math.pow(a, b);
+// }
+
+// // Example usage:
+// const result = power(2, 3); // 2^3 = 8
+// console.log(result);
+
+
+                                   // ! Q  No 2 
+                                   
+                                   
+// function isLeapYear(year) {
+// if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+// return true;
+// } else {
+// return false;
+// }
+// }
+// const yearToCheck = 2021;
+// if (isLeapYear(yearToCheck)) {
+// document.write(`${yearToCheck} is a leap year.`);
+// } else {
+// document.write(`${yearToCheck} is not a leap year.`);
+// }
+                                  
+                                  // ! Q No 3
+                                  
+                                  
+// function calculateSemiPerimeter(a, b, c) {
+// return (a + b + c) / 2;
+// }
+                                  
+// function calculateTriangleArea(a, b, c) {
+// const s = calculateSemiPerimeter(a, b, c);
+// const area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+// return area;
+// }
+
+// const sideA = 5;
+// const sideB = 6;
+// const sideC = 7;
+
+// const area = calculateTriangleArea(sideA, sideB, sideC);
+// document.write(`The area of the triangle is: ${area}`);
+                                  
+                                // ! Q No : 4
+
+
+
+// function calculateAverageAndPercentage(mark1, mark2, mark3) {
+// const average = (mark1 + mark2 + mark3) / 3;
+// const percentage = ((mark1 + mark2 + mark3) / (3 * 100)) * 100;
+// return { average, percentage };
+// }
+                                    
+// function mainFunction(mark1, mark2, mark3) {
+// const { average, percentage } = calculateAverageAndPercentage(mark1, mark2, mark3);
+                                    
+// document.write("Marks:");
+// document.write("Subject 1: " + mark1);
+// document.write("Subject 2: " + mark2);
+// document.write("Subject 3: " + mark3);
+// document.write("Average: " + average);
+// document.write("Percentage: " + percentage + "%");
+// }
+                                    
+// mainFunction(85, 90, 78);
+
+
+
+                          // ! Q No : 5
+                          
+                          
+
+// function customIndexOf(inputString, searchChar) {
+// for (let i = 0; i < inputString.length; i++) {
+// if (inputString[i] === searchChar) {
+// return i; 
+// }
+// }
+// return -1; 
+// }
+                              
+// const str = "Hello, World!";
+// const searchChar = "o";
+// const index = customIndexOf(str, searchChar);
+// if (index !== -1) {
+// console.log(`'${searchChar}' found at index ${index}`);
+// } else {
+// console.log(`'${searchChar}' not found in the string.`);
+// }
+                              
+                          
+                          // ! Q No: 6
+
+// function removeVowels(sentence) {
+//   return sentence.replace(/[aeiouAEIOU]/g, "");
+// }
+// const sentence = "This is a sample sentence.";
+// const result = removeVowels(sentence);
+// console.log(result); 
+
+                          
+                        //  ! Q No : 7
+
+
+function pagal (){
+return 
+
+}
+
